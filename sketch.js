c01ad4f02967d9ccf5b935d3ab793478743d2bde
Keelie-Keelie <78715386+Keelie-Keelie@users.noreply.github.com>
2021-07-06 16:07:09 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -71,8 +71,8 @@ function draw() {
   rectMode(CENTER);
   background(0);
  
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+  //packageSprite.x= packageBody.position.x 
+  //packageSprite.y= packageBody.position.y 
 
   
   drawSprites();
